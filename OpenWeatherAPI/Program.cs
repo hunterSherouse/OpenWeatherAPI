833a7c53f93c46fd10f623de1ef22346ff9a9f2f
hunterSherouse <huntersherouse2@gmail.com>
2021-05-04 14:01:03 -0500

    initial commit

---
+++ OpenWeatherAPI/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace OpenWeatherAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            Console.WriteLine("Please enter your API Key:");
+            var apiKey = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Please enter a city name: ");
+                var cityName = Console.ReadLine();
+                var weatherURL = $"http://api.openweathermap.org/data/2.5/weather?q={cityName}&units=imperial&appid={apiKey}";
+
+                string response = client.GetStringAsync(weatherURL).Result;
+                var formattedResponse = JObject.Parse(response).GetValue("main").ToString();
+                Console.WriteLine(formattedResponse);
+                Console.WriteLine();
+                Console.WriteLine("Would you like to choose a different city?");
+                var userInput = Console.ReadLine();
+                Console.WriteLine();
+                if (userInput.ToLower() == "no")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
